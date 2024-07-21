085b8180f71b436d745957cfea373ebddd170a60
Daniel van Dijk <noreply+daniel@git.vandijk.sh>
2024-07-21 16:04:48 +0200

    chore: re-add package.json and update .prettierrc
    Refs: #2

--- packages/prettier/index.js
+++ packages/prettier/index.js
@@ -1,24 +1,24 @@
 module.exports = {
-  "printWidth": 100,
-  "trailingComma": "es5",
-  "tabWidth": 2,
-  "useTabs": false,
-  "semi": true,
-  "singleQuote": false,
-  "quoteProps": "consistent",
-  "jsxSingleQuote": false,
-  "bracketSpacing": true,
-  "arrowParens": "always",
-  "requirePragma": false,
-  "proseWrap": "preserve",
-  "htmlWhitespaceSensitivity": "css",
-  "endOfLine": "lf",
-  "overrides": [
+  printWidth: 100,
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "consistent",
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: "always",
+  requirePragma: false,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  overrides: [
     {
-      "files": ".*rc",
-      "options": {
-        "parser": "json"
-      }
-    }
-  ]
-}
+      files: ".*rc",
+      options: {
+        parser: "json",
+      },
+    },
+  ],
+};
